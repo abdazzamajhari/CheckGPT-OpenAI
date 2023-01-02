0acaa3e5cd159a5b82ca484ac7896be1f1ff8720
Abdul Azzam Ajhari <42313402+abdazzamajhari@users.noreply.github.com>
2023-01-02 08:27:33 +0700

    update v1.0.0

--- app.py
+++ app.py
@@ -8,29 +8,36 @@ from googletrans import Translator
 # Flask constructor
 app = Flask(__name__)  
  
+@app.route('/')
+def home():
+    return render_template('form.html')
+
 # A decorator used to tell the application
 # which URL is associated function
-@app.route('/', methods =["GET", "POST"])
-def gfg():
+@app.route('/predict', methods =["GET", "POST"])
+def predict():
     if request.method == "POST":
        # getting input with input in HTML form
-       words = request.form.get("input")
+      #  words = request.form.get("input")
+       words = request.form
+       for k, v in words.items():
+          val = v
 
        tokenizer = AutoTokenizer.from_pretrained("roberta-base-openai-detector")
        model = AutoModelForSequenceClassification.from_pretrained("roberta-base-openai-detector")
 
        translator = Translator()
-       translated_text = translator.translate(words, dest='en')
-       
+       translated_text = translator.translate(val, dest='en')
+         
        inputs = tokenizer(translated_text.text, return_tensors="pt")
        with torch.no_grad():
-            logits = model(**inputs).logits
-       
+          logits = model(**inputs).logits
+         
        predicted_class_id = logits.argmax().item()
        results = model.config.id2label[predicted_class_id]
 
-       return "The result is "+results
-    return render_template("form.html")
+      #  return "Hasilnya adalah "+results
+    return render_template("form.html", val=translated_text, results=results)
  
 if __name__=='__main__':
    app.run()
